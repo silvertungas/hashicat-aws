4224c4df3592f78f11ff54ea35da73d13f7d1775
silvertungas <46852640+silvertungas@users.noreply.github.com>
2023-11-23 14:35:00 +0100

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Ready to get right catted up? ${prefix} got u
+  Ready to get right catted up? ${PREFIX} got u
   <!-- END -->
 
   </div>
